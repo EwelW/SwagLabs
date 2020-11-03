5cba1ba9bd295588249370d95055a9758839ff65
EwelW <67927067+EwelW@users.noreply.github.com>
2020-11-04 00:01:35 +0100

    Add files via upload

--- SwagLabs.java
+++ SwagLabs.java
@@ -1,10 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -19,16 +20,16 @@ public class SwagLabs{
     }
 
     @Test
-    public void addOneToCart() throws InterruptedException {
+    public void addOneToCart() throws InterruptedException, IOException {
         driver.navigate().to("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
         Thread.sleep(3000);
-        List<WebElement> prod = driver.findElements(By.className("inventory_item"));
-        for (int i = 0; i < prod.size(); i++) {
-            String productName = prod.get(i).getText();
+        List<WebElement> productList = driver.findElements(By.className("inventory_item"));
+        for (int i = 0; i < productList.size(); i++) {
+            String productName = productList.get(i).getText();
 
 
             if (productName.contains("Sauce Labs Backpack")) {
@@ -40,6 +41,8 @@ public class SwagLabs{
         System.out.println("Price: " +driver.findElement(By.className("inventory_item_price")).getText()+"$");
         System.out.println("Qty: "+driver.findElement(By.className("cart_quantity")).getText());
         System.out.println("One product was added to the cart");
+        File scrn=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrn, new File("C://Users//Dell//IdeaProjects/Scrn.png"));
     }
 
 
